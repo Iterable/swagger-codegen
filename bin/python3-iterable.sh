078fdc32100c1a6bf8f223c321fc2128a7489466
Girish Sastry <g.sastry@gmail.com>
2013-09-02 20:11:30 -0700

    Adds python and python3 gen scripts.

---
+++ bin/python3-iterable.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+SCRIPT="$0"
+ENV="$1"
+API_KEY="iterableApiKey"
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` add 'prod' to generate api from prod"
+  exit 0
+fi
+
+echo $ENV
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+cd $APP_DIR
+
+echo $APP_DIR
+echo $@
+
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+
+if [ "$ENV" = "prod" ]; then
+  ags="$@ com.iterable.codegen.Python3 http://api.iterable.com/api-docs.json $API_KEY"
+else
+  ags="$@ com.iterable.codegen.Python3 http://localhost:9000/api-docs.json $API_KEY"
+fi
+
+
+if [ -f $APP_DIR/target/scala-2.9.1/swagger-codegen.jar ]; then
+  scala -cp target/scala-2.9.1/swagger-codegen.jar $ags
+elif [[ -f $APP_DIR/target/scala-2.10/swagger-codegen.jar  ]]; then
+  scala -cp target/scala-2.10/swagger-codegen.jar $ags
+else
+  ./sbt assembly
+  scala -cp target/swagger-codegen.jar $ags
+fi
+
